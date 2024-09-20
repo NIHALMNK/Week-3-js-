f86970105fdef5070e28ff6b72cccfe0dae86c9c
NIHAL <keedathnihal@gmail.com>
2024-09-20 11:49:38 +0530

    js2

--- objects/prototype.js
+++ objects/prototype.js
@@ -5,6 +5,7 @@ function person(name){
 person.prototype.age=20;
 
 const p1= new person("nihal");
-console.log(p1);//=>person { name: 'nihal', age: 20 }
+console.log(p1);//=>person { name: 'nihal'}
 
 console.log(p1.age)//=> 20
+//=======================================================
